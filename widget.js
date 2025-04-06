a27195da6042be0191aafe1c27bafc133fb3194a
Rajkumar0323 <140163703+Rajkumar0323@users.noreply.github.com>
2025-04-07 00:42:09 +0530

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,1 +1,23 @@
+//Should be in assests as main.js
+document.addEventListener("variant:change", (event) => {
+  const variant = event.detail.variant;
 
+  const stockText = document.querySelector(".variant-stock-widget");
+
+  if (variant.inventory_quantity > 0) {
+    stockText.innerHTML = `✅ In stock: ${variant.inventory_quantity} items left`;
+  } else {
+    stockText.innerHTML = `❌ Out of Stock`;
+  }
+});
+document.addEventListener("variant:change", (event) => {
+  const variant = event.detail.variant;
+
+  const stockText = document.querySelector(".variant-stock-widget");
+
+  if (variant.inventory_quantity > 0) {
+    stockText.innerHTML = `✅ In stock: ${variant.inventory_quantity} items left`;
+  } else {
+    stockText.innerHTML = `❌ Out of Stock`;
+  }
+});
