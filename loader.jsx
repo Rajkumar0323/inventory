d349a4c0b9bc5836d9924e825333c0f84c31e478
Rajkumar0323 <140163703+Rajkumar0323@users.noreply.github.com>
2025-04-07 00:44:02 +0530

    Update loader.jsx

--- loader.jsx
+++ loader.jsx
@@ -1,1 +1,72 @@
 
+export async function loader({ request }) {
+  const { admin } = await authenticate.admin(request);
+
+  const response = await admin.graphql(
+    `#graphql
+  query {
+    inventoryLevel(id: "gid://shopify/InventoryLevel/118375907570?inventory_item_id=49457295294706") {
+      id
+      quantities(names: ["available", "incoming", "committed", "damaged", "on_hand", "quality_control", "reserved", "safety_stock"]) {
+        name
+        quantity
+      }
+      item {
+        id
+        sku
+      }
+      location {
+        id
+        name
+      }
+    }
+  }`,
+  );
+
+  const data = await response.json();
+
+  const response1 = await admin.graphql(`
+      query {
+        inventoryItems(first: 5) {
+          edges {
+            node {
+              id
+              sku
+              tracked
+              variant {
+                id
+                title
+                product {
+                  id
+                  title
+                  featuredImage {
+                    url
+                    altText
+                  }
+                }
+              }
+              inventoryLevels(first: 5) {
+          edges {
+            node {
+              id
+              quantities(names: ["available", "incoming", "committed", "damaged", "on_hand", "quality_control", "reserved", "safety_stock"]) {
+                name
+                quantity
+              }
+              location {
+                id
+                name
+              }
+            }
+          }
+        }
+            }
+          }
+        }
+      }
+    `);
+
+  const data1 = await response1.json();
+
+  return json({ data, data1 });
+}
